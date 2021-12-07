ac07f0e4790f4a99ec61a33c22bfd404742abb79
folarin oyenuga <folami25@gmail.com>
2021-12-07 13:45:20 +0100

    an image recognition model

---
+++ brainy.py
@@ -0,0 +1,16 @@
+from imageai.Classification import ImageClassification
+import os
+# below is the execution path which is the cmd pwd
+execution_path = os.getcwd()
+
+# instantiate the image prediction
+prediction = ImageClassification()
+prediction.setModelTypeAsMobileNetV2()
+prediction.setModelPath(os.path.join(execution_path, "mobilenet_v2.h5"))
+prediction.loadModel()
+
+
+# make a prediction
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "giraffe.jpg"), result_count=5 )
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
